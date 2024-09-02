845a60dee201677967ce65f29507f198799ef0cd
HeikoAydt <heiko.aydt@sec.ethz.ch>
2024-09-02 11:14:52 +0800

    Update meta.py

--- saas/meta.py
+++ saas/meta.py
@@ -1,4 +1,4 @@
-__version__ = '0.7.0-alpha'
+__version__ = '1.0.0-alpha'
 __name__ = 'saas-middleware'
 __title__ = 'SaaS Middleware'
 __description__ = 'Middleware for powering digital twin federations of models'
