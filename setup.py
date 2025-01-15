44e55532351e129fde3746259e1f4b849f6b6d50
HeikoAydt <heiko.aydt@sec.ethz.ch>
2025-01-15 08:50:26 +0800

    Remove obsolete files

--- setup.py
+++ setup.py
@@ -47,7 +47,6 @@ setup(
     },
     classifiers=[
         'Programming Language :: Python :: 3'
-        'License :: OSI Approved :: MIT License'
         'Operating System :: OS Independent'
     ],
 )
